16933a6b65c0c3e1264efe0367f94c05ed4d49ff
LionetChen <chinalionet@gmail.com>
2023-03-03 17:36:19 +1100

    Adding chapter 2

---
+++ AsyncLearning/TaskWithContinuation/Program.cs
@@ -0,0 +1,19 @@
+﻿Task<string> antecedent = new(() =>
+{
+    Task.Delay(2000).Wait();
+    return DateTime.Today.ToShortDateString();
+});
+
+// If antecedent is not started, continuation.Result will stuck and result in a deadlock
+antecedent.Start();
+
+// Define the continuation. But it will not execute until antecedent is finished.
+Task<string> continuation = antecedent.ContinueWith(
+    ant =>
+    {
+        return $"Today is {ant.Result}";
+    }
+);
+
+Console.WriteLine("This should be shown before any tasks are done.");
+Console.WriteLine(continuation.Result);
